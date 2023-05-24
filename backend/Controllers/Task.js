0bb811485834f799f5d74f3e7aabf05b16a9b444
JuanPabloParra <juan_pab.parra@uao.edu.co>
2023-05-24 17:26:04 -0500

    challenge18

--- backend/Controllers/Task.js
+++ backend/Controllers/Task.js
@@ -1,23 +1,95 @@
 const crearTask = async (req, res = express.Response) => {
-    const task = new Task(req.body);
-    try {
-      task.user = req.uid;
-      const saved = await task.save();
-      res.json({
-        ok: true,
-        task: saved
-      });
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({
+  const task = new Task(req.body);
+  try {
+    task.user = req.uid;
+    const saved = await task.save();
+    res.json({
+      ok: true,
+      task: saved
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      task: 'Internal Error'
+    });
+  }
+};
+
+const listarTasks = async (req, res = express.Response) => {
+  try {
+    const tasks = await Task.find().populate('user', 'name');
+    res.status(200).json({
+      ok: true,
+      tasks
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      task: 'Error Interno'
+    });
+  }
+};
+
+const actualizarTask = async (req, res) => {
+  try {
+    const taskId = req.params.id;
+    const updatedData = req.body;
+
+    // Buscar la tarea por su ID y actualizar los campos con los datos proporcionados
+    const task = await Task.findByIdAndUpdate(taskId, updatedData, { new: true });
+
+    if (!task) {
+      return res.status(404).json({
         ok: false,
-        task: 'Internal Error'
+        error: 'Tarea no encontrada'
       });
     }
-  };
-  module.exports = {
-    crearTask
-    
-  };
-  
-  
+
+    res.json({
+      ok: true,
+      task
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      error: 'Error interno'
+    });
+  }
+};
+
+const eliminarTask = async (req, res) => {
+  try {
+    const taskId = req.params.id;
+
+    // Buscar la tarea por su ID y eliminarla
+    const deletedTask = await Task.findByIdAndDelete(taskId);
+
+    if (!deletedTask) {
+      return res.status(404).json({
+        ok: false,
+        error: 'Tarea no encontrada'
+      });
+    }
+
+    res.json({
+      ok: true,
+      message: 'Tarea eliminada correctamente'
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      error: 'Error interno'
+    });
+  }
+};
+
+module.exports = {
+  crearTask,
+  listarTasks,
+  actualizarTask,
+  eliminarTask
+};
--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 require('dotenv').config();
 const { dbConnection } = require('./database/config');
 const cors = require('cors');
+const TaskScheme = require('./models/TaskScheme'); // Asegúrate de importar el modelo de Task
 
 const app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static('public'));
 app.use(express.json());
 
 app.use('/api/auth', require('./routes/auth'));
+app.use('/api/task', require('./routes/task'));
 
 app.listen(process.env.PORT, () => {
   console.log('Servidor corriendo en puerto', process.env.PORT);
